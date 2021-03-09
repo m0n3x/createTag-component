e45e94655b114f0d650161e58b6ce149a58b14e6
Konstantin Semenenko <55196388+m0n3x@users.noreply.github.com>
2021-03-09 23:06:58 +0200

    fix: redesigned readonly mode functionality

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //hints
 console.group('How to use');
 console.log("To get a list of tags use %cgetTags()", "color: blue");
-console.log("To set a list of tags use %csetTags('tag1, tag2..')", "color: blue");
+console.log("To set a list of tags use %csetTags(['tag1', 'tag2', ...])", "color: blue");
 console.log("To add tag use %caddTag('tag')", "color: blue");
 console.log("if you want to delete tag by name use %cremoveTagByName('tag')", "color: blue");
 console.log("if you want to delete tag by number use %cremoveTagByNumber(number)", "color: blue");
@@ -13,13 +13,13 @@ console.groupEnd();
 //get elements
 const btn = document.querySelector('button'),
     input = document.querySelector('.tag'),
-    tagsArea = document.querySelector('.tags-list'),
-    readonlyMode = document.querySelector('#readonlyMode');
+    tagsArea = document.querySelector('.tags-list');
 
 //localstorage functions
 const setLocal = () => localStorage.setItem('tagList', tagsDB.tags);
 const getLocal = () => localStorage.getItem('tagList');
 
+//tags database
 const tagsDB = {
     tags: []
 };
@@ -29,8 +29,9 @@ if (localStorage.getItem('tagList')) {
     localStorage.getItem('tagList').split(',').forEach(i => {
         tagsDB.tags.push(i);
         return tagsDB.tags;
-    })
+    });
 }
+
 // render list of tags
 function createTagsList(tags, parent = tagsArea) {
     parent.innerHTML = '';
@@ -43,94 +44,111 @@ function createTagsList(tags, parent = tagsArea) {
     });
     document.querySelectorAll('.delete').forEach((btn, i) => {
         btn.addEventListener('click', () => {
-            btn.parentElement.remove();
-            tagsDB.tags.splice(i, 1);
-            createTagsList(tags);
-            setLocal();
-        })
-    })
+            if (check) {
+                alert('You can\'t do this in readonly mode');
+            } else {
+                btn.parentElement.remove();
+                tagsDB.tags.splice(i, 1);
+                createTagsList(tags);
+                setLocal();
+            }
+        });
+    });
 }
 
 //add new tag by input
 btn.addEventListener('click', (e) => {
     e.preventDefault();
     if (!input.value) {
-        alert('Введите тег')
+        alert('Enter the tag');
+
     } else {
         let newTag = input.value;
         tagsDB.tags.push(newTag);
-        createTagsList(tagsDB.tags, tagsArea)
+        createTagsList(tagsDB.tags, tagsArea);
         input.value = '';
         setLocal();
     }
 });
 
 //getters/setters
-const setTags = (tagsString) => {
-    if (!tagsString) {
-        throw new Error('Add tags in format "tag1, tag2, ..."')
+const setTags = (...[tagsArr]) => {
+    if (!tagsArr) {
+        throw new Error("Add tags in format ['tag1', 'tag2', ...]");
+    } else if (check) {
+        throw new Error('You can\'t do this in readonly mode');
     } else {
-        [...tagsDB.tags] = tagsString.split(', ');
+        [...tagsDB.tags] = tagsArr;
         createTagsList(tagsDB.tags, tagsArea);
-        localStorage.setItem('tagList', tagsDB.tags)
-
+        localStorage.setItem('tagList', tagsDB.tags);
     }
-}
+};
 const addTag = (tag) => {
     if (!tag) {
-        throw new Error('There are no empty tags :)')
+        throw new Error('There are no empty tags :)');
+    } else if (check) {
+        throw new Error('You can\'t do this in readonly mode');
     } else {
-        tagsDB.tags.push(tag)
+        tagsDB.tags.push(tag);
         createTagsList(tagsDB.tags);
         setLocal();
     }
-}
+};
 const removeTagByNumber = (i) => {
     if (!tagsDB.tags[i - 1]) {
         throw new Error('Tag with this number doesn\'t exist');
+    } else if (check) {
+        throw new Error('You can\'t do this in readonly mode');
     } else {
         createTagsList(tagsDB.tags);
         tagsDB.tags.splice(i - 1, 1);
         createTagsList(tagsDB.tags);
         setLocal();
     }
-}
+};
 const removeTagByName = (tag) => {
     createTagsList(tagsDB.tags);
     if (tagsDB.tags.lastIndexOf(tag) === -1) {
         throw new Error('Invalid tag name');
+    } else if (check) {
+        throw new Error('You can\'t do this in readonly mode');
     } else {
         tagsDB.tags.splice(tagsDB.tags.lastIndexOf(tag), 1);
         console.log(`You've removed tag <${tag}>`);
         createTagsList(tagsDB.tags);
         setLocal();
     }
-
-}
+};
 const getTags = () => {
     return tagsDB.tags.length == 0 ? 'There is no tags' :
         tagsDB.tags.join(', ');
-}
+};
 
 //Readonly mode
-const addReadOnly = (elem) => elem.setAttribute('disabled', 'true');
-const removeReadOnly = (elem) => elem.removeAttribute('disabled');
-const readonlyModeOn = () => {
-    addReadOnly(input);
-    addReadOnly(btn);
-}
-const readonlyModeOff = () => {
-    removeReadOnly(input);
-    removeReadOnly(btn);
-}
-readonlyMode.addEventListener('click', () => {
-    if (readonlyMode.checked) {
-        readonlyModeOn()
+let readonlyMode = document.querySelector('#readonlyMode');
+let check = readonlyMode.checked;
+const funcReadonly = (check) => {
+    if (check) {
+        input.setAttribute('disabled', 'true');
+        btn.setAttribute('disabled', 'true');
     } else {
-        readonlyModeOff()
+        input.removeAttribute('disabled');
+        btn.removeAttribute('disabled');
     }
-})
+};
+readonlyMode.addEventListener('click', () => {
+    check = !check;
+    funcReadonly(check);
+});
+const addReadOnly = () => {
+    check = true;
+    readonlyMode.checked = true;
+    funcReadonly(check);
+};
+const removeReadOnly = () => {
+    check = false;
+    readonlyMode.checked = false;
+    funcReadonly(check);
+};
 
 createTagsList(tagsDB.tags);
-
-
